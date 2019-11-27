c691e1bd807d6a42c99bc77b857c1443d4178ebf
Justin Le <justin@jle.im>
2019-11-26 16:08:16 -0800

    hey, it works?

--- src/GHC/TypeLits/Printf.hs
+++ src/GHC/TypeLits/Printf.hs
@@ -1,4 +1,6 @@
+{-# LANGUAGE AllowAmbiguousTypes    #-}
 {-# LANGUAGE ConstraintKinds        #-}
+{-# LANGUAGE DefaultSignatures      #-}
 {-# LANGUAGE FlexibleInstances      #-}
 {-# LANGUAGE FunctionalDependencies #-}
 {-# LANGUAGE GADTs                  #-}
@@ -15,86 +17,158 @@
 
 module GHC.TypeLits.Printf where
 
--- module GHC.Typelits.Printf (
---   ) where
-
--- import           Data.SOP
+import           Data.Int
 import           Data.Kind
 import           Data.Proxy
+import           Data.Symbol.Utils
 import           Data.Vinyl
 import           Data.Vinyl.Curry
 import           Data.Vinyl.Functor
+import           Data.Word
 import           GHC.Exts
 import           GHC.TypeLits
 import           GHC.TypeLits.Printf.Parse
-
--- so it looks like there are basically three ways of going about this:
---
---
--- | Method | Polymorphic | Type-Determined | Naked types
--- |--------+-------------+-----------------+-------------
--- | A      | x           | x               |
--- | B      |             | x               | x
--- | C      | x           |                 | x
---
--- Ideally we would want all three, but I can't think of a way.  I can only
--- get two out of three.
---
--- The only way maybe would be some sort of "limited" fundep, where ffs
--- determines the *length* or number of arguments of a list but not its
--- contents.
---
--- Of these, I really do like to prioritize polymorphism for usability
--- (using %d for Word), so that leaves A and C: type determined, or naked
--- types.
---
--- Type-determined would guide "type-driven" or "whole-driven" development,
--- and type errors would be a lot nicer.  Naked types would still be type
--- safe! But type errors a little worse.  But, we can maybe make this
--- better by using the custom type error system.
---
--- Ah, there is yet another way: Non-Naked HList, which asks you to feed
--- the items in an HList.  This can be:
---
--- | Method | Polymorphic | Type-Determined | Naked inputs | True polyarity
--- |--------+-------------+-----------------+--------------+----------------
--- | A      | x           | x               |              | x
--- | B      |             | x               | x            | x
--- | C      | x           |                 | x            | x
--- | D      | x           | x               | x            |
---
--- Essentially we hide the non-nakedness by requiring everyone to pass
--- a single "hlist" of items.  This one might actually be better for
--- usability too maybe.  idk.
---
--- So if the main priority is polymorphic, we essentially have three
--- methods now: A (non-naked), C (undetermined), D (not true polyarity).
---
--- Maybe one module with each is better.
---
-
--- data FieldFormat = FF
---     { fmtWidth     :: Maybe Nat
---     , fmtChar      :: Symbol
---     }
-
--- newtype Formatter a = Formatter { runFormatter :: a -> ShowS }
+import           Numeric.Natural
+import qualified Data.Text                 as T
+import qualified Data.Text.Lazy            as TL
+import qualified Text.Printf               as P
 
 -- data CType c = forall a. c a => C a
 
--- class Format (ff :: FieldFormat) c | ff -> c where
---     format :: p ff -> Formatter (CType c)
+class FormatChar (t :: SChar) a where
+    format :: p t -> a -> P.FieldFormat -> ShowS
 
--- instance Format ('FF w "d") Integral where
---     format _ = Formatter $ \case C x -> shows (fromIntegral x :: Integer)
+    default format :: P.PrintfArg a => p t -> a -> P.FieldFormat -> ShowS
+    format _ = P.formatArg
 
--- class RunFormatters (ffs :: [FieldFormat]) fun | ffs -> fun where
---     runFormatters :: p ffs -> String -> fun
+instance FormatChar "c" Char
 
--- instance RunFormatters '[] String where
---     runFormatters _ = id
+instance FormatChar "d" Int
+instance FormatChar "d" Int8
+instance FormatChar "d" Int16
+instance FormatChar "d" Int32
+instance FormatChar "d" Int64
+instance FormatChar "d" Integer
+instance FormatChar "d" Natural
+instance FormatChar "d" Word
+instance FormatChar "d" Word8
+instance FormatChar "d" Word16
+instance FormatChar "d" Word32
+instance FormatChar "d" Word64
 
--- instance (Format ff c, RunFormatters ffs fun) => RunFormatters (ff : ffs) (CType c -> fun) where
---     runFormatters _ str x = runFormatters (Proxy @ffs)
---                           . runFormatter (format (Proxy @ff)) x
---                           $ str
+instance FormatChar "o" Int
+instance FormatChar "o" Int8
+instance FormatChar "o" Int16
+instance FormatChar "o" Int32
+instance FormatChar "o" Int64
+instance FormatChar "o" Integer
+instance FormatChar "o" Natural
+instance FormatChar "o" Word
+instance FormatChar "o" Word8
+instance FormatChar "o" Word16
+instance FormatChar "o" Word32
+instance FormatChar "o" Word64
+
+instance FormatChar "x" Int
+instance FormatChar "x" Int8
+instance FormatChar "x" Int16
+instance FormatChar "x" Int32
+instance FormatChar "x" Int64
+instance FormatChar "x" Integer
+instance FormatChar "x" Natural
+instance FormatChar "x" Word
+instance FormatChar "x" Word8
+instance FormatChar "x" Word16
+instance FormatChar "x" Word32
+instance FormatChar "x" Word64
+
+instance FormatChar "X" Int
+instance FormatChar "X" Int8
+instance FormatChar "X" Int16
+instance FormatChar "X" Int32
+instance FormatChar "X" Int64
+instance FormatChar "X" Integer
+instance FormatChar "X" Natural
+instance FormatChar "X" Word
+instance FormatChar "X" Word8
+instance FormatChar "X" Word16
+instance FormatChar "X" Word32
+instance FormatChar "X" Word64
+
+instance FormatChar "b" Int
+instance FormatChar "b" Int8
+instance FormatChar "b" Int16
+instance FormatChar "b" Int32
+instance FormatChar "b" Int64
+instance FormatChar "b" Integer
+instance FormatChar "b" Natural
+instance FormatChar "b" Word
+instance FormatChar "b" Word8
+instance FormatChar "b" Word16
+instance FormatChar "b" Word32
+instance FormatChar "b" Word64
+
+instance FormatChar "u" Int
+instance FormatChar "u" Int8
+instance FormatChar "u" Int16
+instance FormatChar "u" Int32
+instance FormatChar "u" Int64
+instance FormatChar "u" Integer
+instance FormatChar "u" Natural
+instance FormatChar "u" Word
+instance FormatChar "u" Word8
+instance FormatChar "u" Word16
+instance FormatChar "u" Word32
+instance FormatChar "u" Word64
+
+instance FormatChar "f" Double
+instance FormatChar "f" Float
+
+instance FormatChar "F" Double
+instance FormatChar "F" Float
+
+instance FormatChar "g" Double
+instance FormatChar "g" Float
+
+instance FormatChar "G" Double
+instance FormatChar "G" Float
+
+instance FormatChar "e" Double
+instance FormatChar "e" Float
+
+instance FormatChar "E" Double
+instance FormatChar "E" Float
+
+instance FormatChar "s" String
+instance FormatChar "s" T.Text where
+    format _ = P.formatArg . T.unpack
+instance FormatChar "s" TL.Text where
+    format _ = P.formatArg . TL.unpack
+
+data P (c :: SChar) = forall a. FormatChar c a => P a
+
+class FormatF (ffs :: [Either Symbol FieldFormat]) fun | ffs -> fun where
+    formatF :: p ffs -> String -> fun
+
+instance FormatF '[] String where
+    formatF _ = id
+
+instance (KnownSymbol str, FormatF ffs fun) => FormatF ('Left str ': ffs) fun where
+    formatF _ = formatF (Proxy @ffs)
+                    . (<> symbolVal (Proxy @str))
+
+instance (Reflect ff, ff ~ 'FF f w p m c, FormatF ffs fun) => FormatF ('Right ff ': ffs) (P c -> fun) where
+    formatF _ str (P x) = formatF (Proxy @ffs)
+                              $ str <> format (Proxy @c) x ff ""
+        where
+          ff = reflect (Proxy @ff)
+
+class Printf (str :: Symbol) fun where
+    printf_ :: p str -> fun
+
+instance (Listify str lst, 'Just ffs ~ ParseFmtStr lst, FormatF ffs fun) => Printf str fun where
+    printf_ _ = formatF (Proxy @ffs) ""
+
+printf :: forall str fun. Printf str fun => fun
+printf = printf_ (Proxy @str)
+
--- src/GHC/TypeLits/Printf/Parse.hs
+++ src/GHC/TypeLits/Printf/Parse.hs
@@ -1,9 +1,15 @@
-{-# LANGUAGE KindSignatures       #-}
-{-# LANGUAGE NoStarIsType         #-}
-{-# LANGUAGE TypeFamilies         #-}
-{-# LANGUAGE TypeInType           #-}
-{-# LANGUAGE TypeOperators        #-}
-{-# LANGUAGE UndecidableInstances #-}
+{-# LANGUAGE FlexibleInstances      #-}
+{-# LANGUAGE KindSignatures         #-}
+{-# LANGUAGE LambdaCase             #-}
+{-# LANGUAGE NoStarIsType           #-}
+{-# LANGUAGE RecordWildCards        #-}
+{-# LANGUAGE ScopedTypeVariables    #-}
+{-# LANGUAGE TypeApplications       #-}
+{-# LANGUAGE TypeFamilies           #-}
+{-# LANGUAGE TypeFamilyDependencies #-}
+{-# LANGUAGE TypeInType             #-}
+{-# LANGUAGE TypeOperators          #-}
+{-# LANGUAGE UndecidableInstances   #-}
 
 module GHC.TypeLits.Printf.Parse (
     ParseFmtStr
@@ -12,14 +18,22 @@ module GHC.TypeLits.Printf.Parse (
   , FormatSign(..)
   , WidthMod(..)
   , Flags(..)
+  , EmptyFlags
   , FieldFormat(..)
+  , SChar
+  , Demote
+  , Reflect(..)
   ) where
 
+import           Data.Proxy
+import           Data.Text                           (Text)
+import           GHC.TypeLits hiding                 (natVal)
 import           GHC.TypeLits.Printf.Internal.Parser
-import           GHC.TypeLits
-
-data FormatAdjustment = LeftAdjust | ZeroPad
-data FormatSign       = SignPlus   | SignSpace
+import           GHC.TypeNats
+import           Numeric.Natural
+import           Text.Printf                         (FormatAdjustment(..), FormatSign(..))
+import qualified Data.Text                           as T
+import qualified Text.Printf                         as P
 
 data Flags = Flags
     { fAdjust    :: Maybe FormatAdjustment
@@ -41,9 +55,80 @@ data FieldFormat = FF
     , fmtChar      :: SChar
     }
 
+type family Demote k = a | a -> k
+type instance Demote FormatAdjustment = FormatAdjustment
+type instance Demote FormatSign       = FormatSign
+type instance Demote Bool             = Bool
+type instance Demote (Maybe a)        = Maybe (Demote a)
+type instance Demote Nat              = Natural
+type instance Demote Symbol           = Text
+type instance Demote Flags            = Flags
+type instance Demote WidthMod         = WidthMod
+type instance Demote FieldFormat      = P.FieldFormat
+
+class Reflect (x :: a) where
+    reflect :: p x -> Demote a
+
+instance Reflect 'LeftAdjust where
+    reflect _ = LeftAdjust
+instance Reflect 'ZeroPad where
+    reflect _ = ZeroPad
+instance Reflect 'SignPlus where
+    reflect _ = SignPlus
+instance Reflect 'SignSpace where
+    reflect _ = SignSpace
+instance Reflect 'WMhh where
+    reflect _ = WMhh
+instance Reflect 'WMh where
+    reflect _ = WMh
+instance Reflect 'WMl where
+    reflect _ = WMl
+instance Reflect 'WMll where
+    reflect _ = WMll
+instance Reflect 'WML where
+    reflect _ = WML
+instance Reflect 'False where
+    reflect _ = False
+instance Reflect 'True where
+    reflect _ = True
+instance Reflect 'Nothing where
+    reflect _ = Nothing
+instance Reflect x => Reflect ('Just x) where
+    reflect _ = Just (reflect (Proxy @x))
+instance KnownNat n => Reflect (n :: Nat) where
+    reflect = natVal
+instance KnownSymbol n => Reflect (n :: Symbol) where
+    reflect = T.pack . symbolVal
+instance (Reflect d, Reflect i, Reflect l) => Reflect ('Flags d i l) where
+    reflect _ = Flags (reflect (Proxy @d))
+                      (reflect (Proxy @i))
+                      (reflect (Proxy @l))
+instance (Reflect flags, Reflect width, Reflect prec, Reflect mods, Reflect chr)
+      => Reflect ('FF flags width prec mods chr) where
+    reflect _ = P.FieldFormat{..}
+      where
+        Flags{..}    = reflect (Proxy @flags)
+        fmtWidth     = fromIntegral <$> reflect (Proxy @width)
+        fmtPrecision = fromIntegral <$> reflect (Proxy @prec)
+        fmtAdjust    = fAdjust
+        fmtSign      = fSign
+        fmtAlternate = fAlternate
+        fmtModifiers = foldMap modString (reflect (Proxy @mods))
+        fmtChar      = T.head (reflect (Proxy @chr))
+
+
+modString :: WidthMod -> String
+modString = \case
+    WMhh -> "hh"
+    WMh  -> "h"
+    WMl  -> "l"
+    WMll -> "ll"
+    WML  -> "L"
 
 data FlagParser :: Parser Flags
-type instance RunParser FlagParser str = 'Just (ProcessFlags ('Flags 'Nothing 'Nothing 'False) str)
+type instance RunParser FlagParser str = 'Just (ProcessFlags EmptyFlags str)
+
+type EmptyFlags = 'Flags 'Nothing 'Nothing 'False
 
 type family ProcessFlags (f :: Flags) (str :: [SChar]) :: (Flags, [SChar]) where
     ProcessFlags ('Flags d i l) ("-" ': cs) = '( 'Flags ('Just (UpdateAdjust d 'LeftAdjust)) i l, cs)
