717ed9250bde2a736a357673b5a50646213718bc
justin <justin@jle.im>
2024-09-09 21:35:20 -0700

    apply formatter to new code

--- src/GHC/TypeLits/Printf/Internal/Unsatisfiable.hs
+++ src/GHC/TypeLits/Printf/Internal/Unsatisfiable.hs
@@ -1,16 +1,16 @@
-{-# language DataKinds #-}
-{-# language FlexibleContexts #-}
-{-# language FlexibleInstances #-}
-{-# language KindSignatures #-}
-{-# language MultiParamTypeClasses #-}
-{-# language UndecidableInstances #-}
-{-# language UndecidableSuperClasses #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE KindSignatures #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE UndecidableInstances #-}
+{-# LANGUAGE UndecidableSuperClasses #-}
 
 -- | A partial mock-up of the proposed Unsatisfiable constraint.
-module GHC.TypeLits.Printf.Internal.Unsatisfiable
-  ( Unsatisfiable
-  , unsatisfiable
-  ) where
+module GHC.TypeLits.Printf.Internal.Unsatisfiable (
+  Unsatisfiable,
+  unsatisfiable,
+) where
 
 import GHC.TypeLits
 
