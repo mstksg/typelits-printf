1dc212f978312ed605c5088a7dc8b0481cc9980c
justin <justin@jle.im>
2024-09-11 21:45:15 -0700

    formatting

--- test/spec.hs
+++ test/spec.hs
@@ -16,7 +16,8 @@ main = hspec do
       printf "Hello %0.2f, %s" (3.26 :: Double) "Luigi" `shouldBe` "Hello 3.26, Luigi"
       printf' @"Hello %0.2f, %s" (3.26 :: Double) "Luigi" `shouldBe` "Hello 3.26, Luigi"
     it "formats a long string" do
-      printf "This is a long string a very %s in the past this took a long time to compile %d"
+      printf
+        "This is a long string a very %s in the past this took a long time to compile %d"
         "long string"
         (2 :: Int)
         `shouldBe` "This is a long string a very long string in the past this took a long time to compile 2"
